fd4002f1aee98a8702d6a08be2a01a806e1754c0
TomCat <tomcat@tomcatstours.com>
2021-06-12 08:44:15 -0400

    Combined dungeonmaps.lua and mapinfo.lua into maps.lua

--- addonSrc/classic/data/maps.lua
+++ addonSrc/classic/data/maps.lua
@@ -2,6 +2,7 @@ local addonName, addon = ...
 
 local C_Map = C_Map
 local CreateVector2D = CreateVector2D
+local UnitFactionGroup = UnitFactionGroup
 
 local mapInfos = {
 	[33] = { nil, nil, 209, "ShadowfangKeep", nil, 1, false },
@@ -247,4 +248,45 @@ function addon.GetDungeonsForMap(mapID)
 		return mapInstance
 	end
 	return { }
+end
+
+local mapDropDownOverrides = {
+	[1459] = true, -- battleground
+	[1460] = true, -- battleground
+	[1461] = true, -- battleground
+}
+
+--for _, v in pairs(addon.mapInfos) do
+--	if (v.enabled) then
+--		addon.mapDropDownOverrides[v.mapID] = true
+--	end
+--end
+
+local mapInfoOverrides = {
+	[1459] = { parentMapID = 1424 },
+	[1460] = {
+		Horde = { parentMapID = 1413 },
+		Alliance = { parentMapID = 1440 },
+	},
+	[1461] = { parentMapID = 1417 },
+}
+
+function addon.GetMapInfo(mapID)
+	return addon.GetDungeonMapInfo(mapID)
+end
+
+function addon.GetMapInfoByInstanceID(instanceID)
+	return addon.GetDungeonMapInfoByInstanceID(instanceID)
+end
+
+function addon.GetMapInfoOverride(mapID)
+	local override = mapInfoOverrides[mapID]
+	if (override) then
+		local faction = UnitFactionGroup("player")
+		return override[faction] or override
+	end
+end
+
+function addon.IsMapDropDownOverridden(mapID)
+	return mapDropDownOverrides[mapID] and true or false
 end
