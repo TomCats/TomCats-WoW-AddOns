01fc5bb0d0c4336c6a2367ca352e720580ae8819
TomCat <tomcat@tomcatstours.com>
2021-08-16 12:51:54 -0400

    Minimap instance difficulty override code

--- addonSrc/classic/default_saved_variables.lua
+++ addonSrc/classic/default_saved_variables.lua
@@ -1,9 +1,12 @@
 --[[ See license.txt for license and copyright information ]]
 local _, addon = ...
 
+local HINT_ALL = addon.constants.HINT_ALL
+
 addon.defaultSavedVariables = {
 	TomCats_Account = {
-		lastExpirationWarning = 0
+		lastExpirationWarning = 0,
+		minimapInstanceDifficultyAnchorOverride = addon.constants.HINT_ALL,
 	},
 	TomCats_Character = {
 		miniWorldMap = "1",
