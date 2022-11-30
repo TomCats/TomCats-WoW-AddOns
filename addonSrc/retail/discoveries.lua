ac0e4f2a6e9af292f554f90d6480ba8aeff34ebf
TomCat <tomcat@tomcatstours.com>
2022-11-30 11:34:53 -0500

    Updated client DB version to latest

--- addonSrc/retail/discoveries.lua
+++ addonSrc/retail/discoveries.lua
@@ -565,13 +565,13 @@ local function OnEvent(event, arg1)
 		if (addonName == arg1) then
 			TomCatsDiscoveryAlertSystem = AlertFrame:AddQueuedAlertFrameSubSystem("TomCatsDiscoveryAlertFrameTemplate", TomCatsDiscoveryAlertFrame_SetUp);
 			--todo: re-enable when going live
-			if (_G["TomCats_Account"].discoveriesVersion ~= "@version@") then
-				_G["TomCats_Account"].discoveries.vignettes = { }
-				_G["TomCats_Account"].discoveries.vignetteAtlases = { }
-				_G["TomCats_Account"].discoveries.version = "@version@"
-				_G["TomCats_Account"].discoveriesResetCount = 0
-				_G["TomCats_Account"].discoveriesVersion = "@version@"
-			end
+			--if (_G["TomCats_Account"].discoveriesVersion ~= "@version@") then
+			--	_G["TomCats_Account"].discoveries.vignettes = { }
+			--	_G["TomCats_Account"].discoveries.vignetteAtlases = { }
+			--	_G["TomCats_Account"].discoveries.version = "@version@"
+			--	_G["TomCats_Account"].discoveriesResetCount = 0
+			--	_G["TomCats_Account"].discoveriesVersion = "@version@"
+			--end
 			local discoveries = 0
 			discoveredVignettes = _G["TomCats_Account"].discoveries.vignettes
 			for _ in pairs(discoveredVignettes) do
