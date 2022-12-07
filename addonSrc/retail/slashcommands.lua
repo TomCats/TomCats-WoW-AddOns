430cdb280f68400bf1a34a2beb9e43e8bce8fd99
TomCat <tomcat@tomcatstours.com>
2022-12-07 04:16:00 -0500

    Taint-safe slash commands

--- addonSrc/retail/slashcommands.lua
+++ addonSrc/retail/slashcommands.lua
@@ -6,19 +6,53 @@ local function setDefaultVignetteIcon(iconname)
 end
 
 local function handleSlashCommand(msg)
-	if (msg == "") then
+	if (msg == "/TOMCAT" or msg == "/TOMCATS") then
 		Settings.OpenToCategory(TomCats_Config.category:GetID())
 	else
 		local t={}
 		for str in string.gmatch(msg, "([^ ]+)") do
 			table.insert(t, str)
 		end
-		if (t[1] == "icon") then
+		if (t[2] == "icon") then
 			setDefaultVignetteIcon(select(2, unpack(t)))
 		end
 	end
 end
 
-SLASH_TOMCATS1 = "/TOMCATS"
-SLASH_TOMCATS2 = "/TOMCAT"
-SlashCmdList["TOMCATS"] = handleSlashCommand
+-- Work-around for Blizzard's issue where slash commands are allowing taint to spread
+local lastMessage
+
+local function starts(String,Start)
+	return string.sub(String,1,string.len(Start))==Start
+end
+
+hooksecurefunc("ChatEdit_ParseText", function(editBox, send, parseIfNoSpaces)
+	if (send == 0) then
+		lastMessage = editBox:GetText()
+	end
+end)
+
+hooksecurefunc("ChatFrame_DisplayHelpTextSimple", function(frame)
+	if (lastMessage and lastMessage ~= "") then
+		local cmd = string.upper(lastMessage)
+		if (starts(cmd, "/TOMCAT")) then
+			local count = 1
+			local numMessages = frame:GetNumMessages()
+			local function predicateFunction(entry)
+				if (count == numMessages) then
+					if (entry == HELP_TEXT_SIMPLE) then
+						return true
+					end
+				end
+				count = count + 1
+			end
+			frame:RemoveMessagesByPredicate(predicateFunction)
+			handleSlashCommand(cmd)
+		end
+	end
+end)
+
+--
+--SLASH_TOMCATS1 = "/TOMCATS"
+--SLASH_TOMCATS2 = "/TOMCAT"
+--SlashCmdList["TOMCATS"] = handleSlashCommand
