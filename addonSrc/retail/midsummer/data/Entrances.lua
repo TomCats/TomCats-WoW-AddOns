ab84c2b56beb56383217dc24d073b316d9060539
TomCat <tomcat@tomcatstours.com>
2023-06-25 23:34:54 -0400

    Midsummer: Improved accuracy/fixed some locations

--- addonSrc/retail/midsummer/data/Entrances.lua
+++ addonSrc/retail/midsummer/data/Entrances.lua
@@ -16,7 +16,7 @@ if (PlayerFaction == "Horde") then
             { "Entrance ID", "Quest IDs", "UIMap ID", "Location", "Type", "Add to Parent UIMaps" },
             {
                 { 1, {11862}, 2070, Coords(0.5722,0.5175), TYPE_PHASED, false }, --TirisFal Glades alt phase work-around
-                { 2, {11935}, 57, Coords(0.3501,0.4713), TYPE_NONE, false }, --Darnassus additional pin
+                { 2, {9332}, 57, Coords(0.3501,0.4713), TYPE_NONE, false }, --Darnassus additional pin
                 { 3, {11933}, 97, Coords(0.2465,0.4941), TYPE_ENTRANCE, false }, --The Exodar additional pin
                 { 4, {28947}, 1527, Coords(0.534,0.32), TYPE_PHASED, false }, -- Uldum additional pin
                 { 5, {28949}, 1527, Coords(0.53,0.34), TYPE_PHASED, false }, --Uldum additional pin
@@ -24,7 +24,7 @@ if (PlayerFaction == "Horde") then
                 { 7, {32509}, 1530, Coords(0.778,0.331), TYPE_PHASED, false }, --Vale additional pin
                 { 8, {9330}, 37, Coords(0.1937,0.3845), TYPE_NONE, false }, --Stormwind additional pin
                 { 9, {9331}, 27, Coords(0.6058,0.3318), TYPE_ENTRANCE, false }, --Ironforge additional pin
-                { 10, {11935, 11753}, 57, Coords(0.5479,0.8826), TYPE_PORTAL_ALLIANCE, true }, --Darnassus additional pin
+                { 10, {9332, 11753}, 57, Coords(0.5479,0.8826), TYPE_PORTAL_ALLIANCE, true }, --Darnassus additional pin
                 { 11, {11933,11735,11738}, 57, Coords(0.5232,0.8947), TYPE_PORTAL_ALLIANCE, true }, --Exodar/Azuremyst/Bloodmyst additional pin (portal)
             }
     )
