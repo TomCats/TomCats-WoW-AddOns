7f216832120796b1f8e0f848031aecf2be619226
TomCat <tomcat@tomcatstours.com>
2023-06-30 01:45:05 -0400

    Midsummer: Improved accuracy of some fire festival locations

--- addonSrc/retail/midsummer/data/Entrances.lua
+++ addonSrc/retail/midsummer/data/Entrances.lua
@@ -38,12 +38,12 @@ else
                 { 3, {9326}, 18, Coords(0.623,0.669), TYPE_NONE, false }, --Undercity additional pin
                 { 4, {9326}, 2070, Coords(0.623,0.669), TYPE_PHASED, false }, --Undercity additional pin
                 { 5, {9325}, 7, Coords(0.3529,0.2425), TYPE_NONE, false }, --Thunder Bluff additional pin
-                { 6, {11935}, 94, Coords(0.5624,0.3737), TYPE_NONE, false }, --Silvermoon additional pin
+                { 6, {11935}, 94, Coords(0.5666,0.4958), TYPE_ENTRANCE, false }, --Silvermoon additional pin
                 { 7, {28948}, 1527, Coords(0.53,0.344), TYPE_PHASED, false }, -- Uldum additional pin
                 { 8, {28950}, 1527, Coords(0.53,0.32), TYPE_PHASED, false }, --Uldum additional pin
                 { 9, {32496}, 1530, Coords(0.779,0.339), TYPE_PHASED, false }, -- Vale additional pin
                 { 10, {32510}, 1530, Coords(0.796,0.372), TYPE_PHASED, false }, --Vale additional pin
-                { 11, {11824}, 57, Coords(0.5232,0.8947), TYPE_PORTAL_ALLIANCE, true }, --Teldrassil additional pin
+                { 11, {11824}, 57, Coords(0.5479,0.8826), TYPE_PORTAL_ALLIANCE, true }, --Teldrassil additional pin
                 { 12, {11806,11809}, 57, Coords(0.5232,0.8947), TYPE_PORTAL_ALLIANCE, true }, --Azuremyst/Bloodmyst additional pin (portal)
             }
     )
