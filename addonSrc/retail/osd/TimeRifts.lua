282c633edcd336803c082f6c016a04f8be2205a5
TomCat <tomcat@tomcatstours.com>
2023-07-13 17:46:50 -0400

    Protection against the TimeRifts POI temporarily being unavailable

--- addonSrc/retail/osd/TimeRifts.lua
+++ addonSrc/retail/osd/TimeRifts.lua
@@ -4,6 +4,11 @@ select(2, ...).SetScope("osd")
 local mapID = 2025 -- Thaldraszus
 local poiID = 7492 -- Time Rift
 local atlasName = "minimap-genericevent-hornicon"
+-- sometimes the POI isn't found for reasons yet to be determined
+local defaultPOI = {
+    name = "Time Rift",
+    widgetSetID = 845
+}
 
 TimeRifts = { }
 
@@ -29,17 +34,17 @@ function TimeRifts.IsVisible()
 end
 
 function TimeRifts.Render(Timers, idx)
-    local poi = C_AreaPoiInfo.GetAreaPOIInfo(mapID, poiID)
+    local poi = C_AreaPoiInfo.GetAreaPOIInfo(mapID, poiID) or defaultPOI
     local timerRow = Timers:GetTimerRow(idx)
     timerRow:SetIcon(atlasName)
     local serverTime = GetServerTime()
     local remainder = serverTime % 3600
     local currentHour = serverTime - remainder
     if (remainder > 900) then
-        timerRow:SetTitle(string.format("Next %s", poi and poi.name or "Time Rift"))
+        timerRow:SetTitle(string.format("Next %s", poi.name))
         timerRow:SetStartTime(currentHour + 3600, 0)
     else
-        timerRow:SetTitle(string.format("%s Ending", poi and poi.name or "Time Rift"))
+        timerRow:SetTitle(string.format("%s Ending", poi.name))
         timerRow:SetStartTime(currentHour + 900, 0)
     end
 
