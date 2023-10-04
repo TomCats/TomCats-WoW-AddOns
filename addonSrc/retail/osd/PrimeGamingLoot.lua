e553cb2d7dc71edc69b80fb578f9b4297e47399a
TomCat <tomcat@tomcatstours.com>
2023-10-04 10:06:35 -0400

    Updated Prime Gaming Loot to announce Zipao Tiger

--- addonSrc/retail/osd/PrimeGamingLoot.lua
+++ addonSrc/retail/osd/PrimeGamingLoot.lua
@@ -2,22 +2,22 @@
 select(2, ...).SetScope("osd")
 
 local atlasName = "VignetteLoot"
-local promotionEndTime = 1695918540
+local promotionEndTime = 1698164940
 local modelFrame
 
 local locale = {
-    ["enUS"] = { lang = "en-us", title1 = "In-game content, free with Prime:", title2 = "\nTabard of Brilliance Transmog\n\nShow off your devotion to the Light with the Tabard of Brilliance.", click = "Click for details" },
-    ["frFR"] = { lang = "fr-fr", title1 = "Contenu en jeu gratuit avec Prime:", title2 = "\nTransmogrification Tabard de la brillance\n\nMontrez votre dévotion pour la Lumière avec le Tabard de brillance.", click = "Cliquer pour les détails" },
-    ["deDE"] = { lang = "de-de", title1 = "Kostenlos mit Prime erhältliche Spielinhalte:", title2 = "\nTransmog vom Waffenrock der Brillanz\n\nZeige mit dem Wappenrock der Brillanz deine Treue zum Licht.", click = "Klicken Sie für weitere Details" },
-    ["esES"] = { lang = "es-es", title1 = "Contenido de juegos, gratis con Prime:", title2 = "\nTransfiguración del Tabardo de resplandor\n\nDemuestra tu devoción a la Luz con el Tabardo de resplandor.", click = "Haga clic para obtener más detalles" },
-    ["esMX"] = { lang = "es-mx", title1 = "Contenido en el juego gratis con Prime:", title2 = "\nTransfiguración Tabardo de resplandor\n\nDemuestra tu devoción a la Luz con el Tabardo de resplandor.", click = "Haga clic para obtener más detalles" },
-    ["itIT"] = { lang = "it-it", title1 = "Contenuti di gioco, gratis con Prime:", title2 = "\nTrasmogrificazione Insegna dell'Acume\n\nMostra la tua devozione alla Luce con l’Insegna dell'Acume.", click = "Clicca per maggiori dettagli" },
-    ["ptBR"] = { lang = "pt-br", title1 = "Conteúdo do jogo grátis com Prime:", title2 = "\nTransmogrificação Tabardo da Inteligência\n\nMostre sua devoção à Luz com o Tabardo da Inteligência.", click = "Clique para mais detalhes" },
-    ["ptPT"] = { lang = "pt-br", title1 = "Conteúdo de jogo, grátis com o Prime:", title2 = "\nTransmog Tabardo de Resplendor\n\nMostra a tua devoção à Luz com o Tabardo de Resplendor.", click = "Clique para mais detalhes" },
+    ["enUS"] = { lang = "en-us", title1 = "In-game content, free with Prime:", title2 = "\nZipao Tiger Pet\n\nPounce into battle with the uniquely colored Zipao Tiger pet.", click = "Click for details" },
+    ["frFR"] = { lang = "fr-fr", title1 = "Contenu en jeu gratuit avec Prime:", title2 = "\nFamilier tigre Zipao\n\nBondissez dans la bataille avec le familier tigre Zipao aux couleurs uniques.", click = "Cliquer pour les détails" },
+    ["deDE"] = { lang = "de-de", title1 = "Kostenlos mit Prime erhältliche Spielinhalte:", title2 = "\nHaustier „Zipao Tiger“\n\nStürze dich mit dem Kampfhaustier „Zipao Tiger“ ins Gefecht.", click = "Klicken Sie für weitere Details" },
+    ["esES"] = { lang = "es-es", title1 = "Contenido de juegos, gratis con Prime:", title2 = "\nMascota Tigre zipao\n\nLánzate a la batalla con la mascota Tigre zipao y su color característico.", click = "Haga clic para obtener más detalles" },
+    ["esMX"] = { lang = "es-mx", title1 = "Contenido en el juego gratis con Prime:", title2 = "\nMascota tigre Zipao\n\nMuestra tus garras en la batalla con la mascota tigre Zipao con un color único.", click = "Haga clic para obtener más detalles" },
+    ["itIT"] = { lang = "it-it", title1 = "Contenuti di gioco, gratis con Prime:", title2 = "\nFamiglio Tigre Zipao\n\nLanciati in battaglia con il famiglio Tigre Zipao dai colori unici.", click = "Clicca per maggiori dettagli" },
+    ["ptBR"] = { lang = "pt-br", title1 = "Conteúdo do jogo grátis com Prime:", title2 = "\nMascote Tigre Zipao\n\nPule para a batalha com o mascote colorido Tigre Zipao.", click = "Clique para mais detalhes" },
+    ["ptPT"] = { lang = "pt-pt", title1 = "Conteúdo de jogo, grátis com o Prime:", title2 = "\nMascote Tigre Zipao\n\nLança-te à batalha com o exclusivamente colorido mascote Tigre Zipao.", click = "Clique para mais detalhes" },
 }
 
 local selectedLocale = locale[GetLocale()] or locale["enUS"]
-local url = "https://gaming.amazon.com/dp/amzn1.pg.item.270f6968-8b67-4923-9c58-4e3aa121ce8d"
+local url = "https://gaming.amazon.com/zipao-tiger-pet/dp/amzn1.pg.item.f647d068-4d89-4d0f-b95d-d2e6810290be"
 
 PrimeGamingLoot = { }
 
@@ -43,7 +43,7 @@ end
 
 function PrimeGamingLoot.IsVisible()
     if (TomCats_Account.preferences.AccessoryWindow.primeGamingLoot == addon.constants.accessoryDisplay.SNOOZED
-            and TomCats_Account.preferences.AccessoryWindow.snoozed["prime_tabardofbrilliance"] == nil) then
+            and TomCats_Account.preferences.AccessoryWindow.snoozed["prime_zipao"] == nil) then
         TomCats_Account.preferences.AccessoryWindow.primeGamingLoot = addon.constants.accessoryDisplay.NOINSTANCES
     end
     if (GetServerTime() > promotionEndTime) then return false end
@@ -74,36 +74,30 @@ function PrimeGamingLoot.Render(Timers, idx)
         if (not modelFrame) then
             modelFrame = CreateFrame("Frame", nil, GameTooltip)
             modelFrame:SetSize(200, 200)
-            local model1 = CreateFrame("DressupModel", nil, modelFrame)
+            --modelFrame:SetPoint("CENTER", GameTooltip, "CENTER")
+            local model1 = CreateFrame("PlayerModel", nil, modelFrame)
             modelFrame.model1 = model1
             model1:SetSize(200, 200)
             local facing = 0
             local maxrads = 6.28319
             local duration = 3
-            Mixin(model1, addon.WardrobeSetsDetailsModelMixin)
-            model1:OnLoad()
             model1:SetScript("OnUpdate", function(self, elapsed)
                 facing = (facing + maxrads * (elapsed / duration)) % maxrads
                 self:SetFacing(facing)
             end)
         end
         GameTooltip_InsertFrame(GameTooltip, modelFrame)
-        modelFrame:SetPoint("RIGHT", GameTooltip, "RIGHT", -30, 0)
-        modelFrame:SetPoint("LEFT", GameTooltip, "LEFT")
+        modelFrame:SetPoint("RIGHT", GameTooltip, "RIGHT", -12, 0);
         local model1 = modelFrame.model1
         model1:SetPoint("CENTER")
-        GameTooltip:Show()
+        model1:SetCreature(36910)
+        model1:SetCamDistanceScale(1.9)
+        modelFrame:Show()
         GameTooltip:AddLine(selectedLocale.click, 1, 1, 1, true)
         numLines = GameTooltip:NumLines()
         line = _G[GameTooltip:GetName().."TextLeft"..numLines]
         line:SetJustifyH("CENTER")
         GameTooltip:Show()
-        modelFrame:Show()
-        model1:Undress()
-        Wear(model1, 3427)
-        Wear(model1, 10035)
-        Wear(model1, 6836)
-        Wear(model1, 38312)
     end
 
     timerRow.clickFunction = function()
@@ -118,9 +112,9 @@ end
 function PrimeGamingLoot.SetVisibilityOption(value)
     TomCats_Account.preferences.AccessoryWindow.primeGamingLoot = value
     if (value == addon.constants.accessoryDisplay.SNOOZED) then
-        TomCats_Account.preferences.AccessoryWindow.snoozed["prime_tabardofbrilliance"] = true
+        TomCats_Account.preferences.AccessoryWindow.snoozed["prime_zipao"] = true
     else
-        TomCats_Account.preferences.AccessoryWindow.snoozed["prime_tabardofbrilliance"] = nil
+        TomCats_Account.preferences.AccessoryWindow.snoozed["prime_zipao"] = nil
     end
     UpdateVisibility()
 end
